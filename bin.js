4250d5d18be92cda4e20c63b78819e9113787ee0
Charles Pletcher <pletcher@users.noreply.github.com>
2024-06-09 10:57:16 -0400

    Update prettier config, get copying working

--- bin.js
+++ bin.js
@@ -2,37 +2,61 @@
 
 import fs from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import * as clack from "@clack/prompts";
 import kleur from "kleur";
 
+import { copy } from "./utils.js";
+
 const packageJson = JSON.parse(
-  fs.readFileSync(new URL("package.json", import.meta.url), "utf-8"),
+    fs.readFileSync(new URL("package.json", import.meta.url), "utf-8"),
 );
 const { version } = packageJson;
 
-let cwd = process.argv[2] || ".";
+let targetDirectory = process.argv[2] || ".";
 
 console.log(kleur.underline(kleur.gray(`create-kodon-app version ${version}`)));
 
 clack.intro(
-  "Welcome to Kōdōn, a set of components and scripts to help you deploy a static commentary with SvelteKit.",
+    "Welcome to Kōdōn, a set of components and scripts to help you deploy a static commentary with SvelteKit.",
 );
 
-if (cwd === ".") {
-  const dir = await clack.text({
-    message: "Where should we create your commentary?",
-    placeholder: " (press Enter to use the current directory)",
-  });
+if (targetDirectory === ".") {
+    const dir = await clack.text({
+        message: "Where should we create your commentary?",
+        placeholder: " (press Enter to use the current directory)",
+    });
 
-  if (clack.isCancel(dir)) {
-    process.exit(0);
-  }
+    if (clack.isCancel(dir)) {
+        process.exit(0);
+    }
 
-  if (dir) {
-    cwd = dir;
-  }
+    if (dir) {
+        targetDirectory = dir;
+    }
 }
 
+if (fs.existsSync(targetDirectory)) {
+    if (fs.readdirSync(targetDirectory).length > 0) {
+        const overwrite = await clack.confirm({
+            message: `${targetDirectory} is not empty. Overwrite contents?`,
+            initialValue: false,
+        });
+
+        if (overwrite !== true) {
+            clack.outro("Refused to overwrite directory. Exiting.");
+            process.exit(0);
+        }
+    }
+}
+
+const sourceDirectory = fileURLToPath(
+    new URL("./template", import.meta.url).href,
+);
+
+copy(sourceDirectory, targetDirectory);
+
 clack.outro(
-  `Check out ${cwd}/README.md for info about writing and deploying your application.`,
+    `Check out ${targetDirectory}/README.md for info about writing and deploying your application.`,
 );
--- utils.js
+++ utils.js
@@ -2,32 +2,32 @@ import fs from "node:fs";
 import path from "node:path";
 
 export function mkdirp(dir) {
-  try {
-    fs.mkdirSync(dir, { recursive: true });
-  } catch (e) {
-    if (e.code === 'EEXIST') {
-      return;
-    }
+    try {
+        fs.mkdirSync(dir, { recursive: true });
+    } catch (e) {
+        if (e.code === "EEXIST") {
+            return;
+        }
 
-    throw e;
-  }
+        throw e;
+    }
 }
 
 export function copy(from, to) {
-  if (!fs.existsSync(from)) {
-    console.error(`Source directory does not exist: ${from}`);
-    return;
-  }
+    if (!fs.existsSync(from)) {
+        console.error(`Source directory does not exist: ${from}`);
+        return;
+    }
 
-  const stat = fs.statSync(from);
+    const stat = fs.statSync(from);
 
-  if (stat.isDirectory()) {
-    fs.readdirSync(from).forEach((f) => {
-      copy(path.join(from, f), path.join(to, f));
-    });
-  } else {
-    mkdirp(path.dirname(to));
+    if (stat.isDirectory()) {
+        fs.readdirSync(from).forEach((f) => {
+            copy(path.join(from, f), path.join(to, f));
+        });
+    } else {
+        mkdirp(path.dirname(to));
 
-    fs.copyFileSync(from, to);
-  }
+        fs.copyFileSync(from, to);
+    }
 }
